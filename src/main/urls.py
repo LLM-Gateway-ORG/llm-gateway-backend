76ff464ee52d6120251a79ddaede7d5b17c1fa80
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-11-02 16:59:17 +0530

    updated routes

--- src/main/urls.py
+++ src/main/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/auth/', include('authentication.urls')),
-    path('api/', include('provider.urls')),
+    path('auth/', include('authentication.urls')),
+    path('provider/', include('provider.urls')),
 ]
 
 # if bool(settings.DEBUG):
