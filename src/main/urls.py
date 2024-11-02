3ea4fdccc722d27d39219a0e67f10b1547c17561
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-11-02 16:59:44 +0530

    uncommented code for media route

--- src/main/urls.py
+++ src/main/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('provider/', include('provider.urls')),
 ]
 
-# if bool(settings.DEBUG):
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
