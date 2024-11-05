a2822852bd6f9124d96b07c10d67bdec123a0a92
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-11-05 21:55:52 +0530

    descrypted api key response

--- src/provider/serializers.py
+++ src/provider/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import ProviderAPIKey
+from provider.helpers import decrypt_value
 
 
 class ProviderAPIKeySerializer(serializers.ModelSerializer):
@@ -10,10 +11,15 @@ class ProviderAPIKeySerializer(serializers.ModelSerializer):
 
 
 class ProviderAPIKeyDetailsSerializer(serializers.ModelSerializer):
+    api_key = serializers.SerializerMethodField()
+
     class Meta:
         model = ProviderAPIKey
         fields = ["id", "provider", "api_key", "created_at"]
         read_only_fields = ["id", "created_at"]
+
+    def get_api_key(self, obj):
+        return decrypt_value(obj.api_key)
 
 
 class ProviderAPIKeyUpdateSerializer(serializers.ModelSerializer):
